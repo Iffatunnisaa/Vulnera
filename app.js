68d20e0a304c53f867d8b5012ced518b73652330
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2025-08-20 11:35:21 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-// app.use(expressLayouts);
-app.use(express.static("public"));
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/login", (req, res) => {
+  res.render("login", { error: "Username atau password salah" });
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
+app.get("/landing", (req, res) => {
+  res.render("landing", { error: null }); // sesuaikan nama file ejs
+});
