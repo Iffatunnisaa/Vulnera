fb37c7cc26330e4c05c631bb82014c7ffcf503b0
azimahhuda <azimahhuda@gmail.com>
2025-08-25 09:40:37 +0700

    auth

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const { Users } = require("./utils/db.js");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -36,6 +38,7 @@ app.use(express.static("public"));
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.user = req.session.user;
   next();
 });
 
@@ -61,6 +64,76 @@ app.get("/register", (req, res) => {
     title: "Daftar",
     messages: req.flash()
   });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const data = {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password,
+    };
+
+    // Cek apakah user sudah ada
+    const existingUser = await Users.findOne({ email: data.email });
+    if (existingUser) {
+      req.flash("error", "Email sudah digunakan. Gunakan email lain!");
+      return res.redirect("/register");
+    }
+
+    // Hash password
+    const saltRounds = 10;
+    data.password = await bcrypt.hash(data.password, saltRounds);
+
+    // Simpan data ke database (cukup sekali)
+    const userdata = await Users.insertOne(data);
+    console.log("User registered:", userdata);
+
+    // Flash + redirect
+    req.flash("success", "Akun telah dibuat! Silakan login.");
+    return res.redirect("/login");
+
+  } catch (err) {
+    console.error(err);
+    req.flash("error", "Terjadi kesalahan. Silakan coba lagi.");
+    return res.redirect("/register");
+  }
+});
+
+app.get("/homepage", (req, res) => {
+  res.render("homepage", {
+    title: "Vulnera | Homepage",
+    messages: req.flash()
+  });
+});
+
+// Login user
+app.post("/login", async (req, res) => {
+  try {
+    const check = await Users.findOne({ email: req.body.email });
+    if (!check) {
+      req.flash("error", "email cannot found");
+      return res.redirect("/login");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      check.password
+    );
+    if (isPasswordMatch) {
+      req.session.user = check; // Simpan seluruh objek pengguna ke sesi
+      req.session.isAuth = true;
+      return res.redirect("/homepage");
+
+    } else {
+      req.flash("error", "wrong password!");
+      return res.redirect("/login");
+    }
+  } catch {
+    req.flash("error", "wrong details!");
+    return res.redirect("/login");
+  }
 });
 
 app.listen(port, () => {
